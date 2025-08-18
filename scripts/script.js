ed37cf058995a83fa353f08278b9a4d621945d4b
JGomez1612 <jostgomez1612@gmail.com>
2025-08-18 18:18:55 -0400

    Removed redundant code, finished choice handling and added fortune display.

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,20 @@
+// Array of fortunes and misfortunes
 const fortunes = {
-    red: [`If you eat something and nobody sees you eat it, it has no calories.`,
-         `You will invent a new dance move in the shower.`,
-          `Error 404: Fortune not found`, 
-          `You need a mint. Like, bad!`],
-    blue: [`You will be hungry again in an hour.`,
-         `Its a good day to have a good day.`, 
-         `Come back later, I'm napping`, 
-         `You will be abducted by aliens, stay indoors!`],
+    red: [`If you eat something and nobody sees you eat it, it has no calories`,
+        `You will invent a new dance move in the shower`,
+        `Uh oh, Error 404: Fortune not found`,
+        `You need a mint. Like, bad!`],
+    blue: [`You will be hungry again in an hour`,
+        `Its a good day to have a good day`,
+        `Come back later, I'm napping`,
+        `You will be abducted by aliens, stay indoors`],
     yellow: [`Your good vibes are scheduled to arrive in 3, 2, 1... NOW!`,
-         `A compliment storm is coming your way!`, 
-         `Your hands will be clammy during an important handshake`, 
-         `It's all downhill from here.`],
-    green: [`You will finally find that thing you weren't looking for.`, 
-        `You will find money in an old pair of pants exactly when you need it.`, 
-        `Your pet is plotting something against you!`, 
+        `A compliment storm is coming your way`,
+        `That group of people was indeed talking about YOU`,
+        `It's all downhill from here`],
+    green: [`You will finally find that thing you weren't looking for`,
+        `You will find money in an old pair of pants exactly when you need it`,
+        `Your pet is plotting something against you`,
         `You might wanna get back in that shower`],
 };
 
@@ -21,10 +22,15 @@ const fortunes = {
 let fieldset = document.querySelector('fieldset');
 let boxes = document.querySelectorAll('.box');
 let nameInput = document.getElementsByName('username')[0];
-let redBox = document.getElementById('red')
-let blueBox = document.getElementById('blue')
-let yellowBox = document.getElementById('yellow')
-let greenBox = document.getElementById('green')
+let redBox = document.getElementById('red');
+let blueBox = document.getElementById('blue');
+let yellowBox = document.getElementById('yellow');
+let greenBox = document.getElementById('green');
+
+// Create fortune and append display
+let fortuneDisplay = document.createElement('div');
+fortuneDisplay.classList.add('fortune');
+fieldset.appendChild(fortuneDisplay);
 
 
 // Create and Append Submit and Reset buttons
@@ -40,23 +46,28 @@ reset.textContent = "Play again";
 reset.style.marginLeft = "10px";
 fieldset.appendChild(reset);
 
+// Point boxes to their fortunes
+const boxIndex = {
+    red: 0,
+    blue: 1,
+    yellow: 2,
+    green: 3,
+};
+
+let selectedColor = null;
+let choosingNumber = false;
+
+
 // Validating Name form
 submit.addEventListener('click', (e) => {
     e.preventDefault()
-    if(nameInput.value.length < 1){
+    const regex = /^[a-zA-Z ]+$/;
+    if (!regex.test(nameInput.value)) {
         nameInput.setCustomValidity("Please fill in a name");
         nameInput.reportValidity()
+        return;
     } else {
         nameInput.setCustomValidity("")
-    }
-});
-
-// Double check validation before moving onto color selection
-submit.addEventListener('click', (e) => {
-    e.preventDefault()
-    if (!nameInput.checkValidity()) {
-        alert("Please fill in a name!");
-        return;
     }
     confirm("Please select a color!");
     submit.disabled = true;
@@ -64,35 +75,52 @@ submit.addEventListener('click', (e) => {
 });
 
 // Box Event Listeners
-for (let box of boxes){
-box.addEventListener('click', handleColorClicks)
+for (let box of boxes) {
+    box.addEventListener('click', handleColorClicks)
 }
 
-// Handling color clicks
-
-function handleColorClicks(e){
+// Handling of user clicks
+function handleColorClicks(e) {
     e.preventDefault();
-
-    if (!nameInput.checkValidity()){
+    if (!nameInput.checkValidity()) {
         alert("Please fill in name before selecting color");
         return;
     }
 
+    const box = e.currentTarget;
+
+    if (!choosingNumber){
+        colorChoice(box);
+    } else if (choosingNumber && selectedColor){
+        numberChoice(box);
+    }
+}
+
+// Choosing color
+function colorChoice(box) {
+    selectedColor = box.id; // store the chosen color
+    choosingNumber = true;
+
     redBox.textContent = "1";
     blueBox.textContent = "2";
     yellowBox.textContent = "3";
     greenBox.textContent = "4";
 
+    submit.textContent = "Now select a number!"
 }
 
-function handleNumberClicks(e){
+// Choosing number
+function numberChoice(box) {
+    const index = boxIndex[box.id];
+    const fortune = fortunes[selectedColor][index];
 
-    redBox.textContent = "1";
-    blueBox.textContent = "2";
-    yellowBox.textContent = "3";
-    greenBox.textContent = "4";
-
+    fortuneDisplay.textContent = ` Hey ${nameInput.value}, ${fortune}!`;
 }
+
+
+
+
+
 
 
 
